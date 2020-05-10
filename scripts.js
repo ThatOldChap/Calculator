24e9e5331abb65f170d891ff41bf2027cd0bd24b
michael <michaeljchap@gmail.com>
2020-05-10 18:18:26 -0400

    Created base files

---
+++ scripts.js
@@ -0,0 +1,8 @@
+/*
+* Developed by Michael Chaplin
+* As a part of the Odin Project Curriculumn Calculator project
+* May XX, 2020
+*/
+
+// Finds the main DOM items to help create the grid
+const colorPickers = document.getElementsByClassName('color-picker');
