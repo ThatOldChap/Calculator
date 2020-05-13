5223771a4ee98efae5ed16ccf95e0b88c0bcef2f
michael <michaeljchap@gmail.com>
2020-05-13 19:09:05 -0400

    Created initial layout

--- scripts.js
+++ scripts.js
@@ -4,5 +4,25 @@
 * May XX, 2020
 */
 
-// Finds the main DOM items to help create the grid
-const colorPickers = document.getElementsByClassName('color-picker');
+// Basic math functions
+const add = (num1, num2) => (num1 + num2);
+const subtract = (num1, num2) => (num1 - num2);
+const multipy = (num1, num2) => (num1 * num2);
+const divide = (num1, num2) => (num1 / num2);
+
+const operate = function (operation, num1, num2) {
+    switch (operation) {
+        case add:
+            return add(num1, num2);    
+        case subtract:
+            return subtract(num1, num2);
+        case multipy:
+            return multipy(num1, num2);  
+        case divide:
+            return divide(num1, num2);      
+        default:
+            return "ERROR";
+    }
+}
+
+
